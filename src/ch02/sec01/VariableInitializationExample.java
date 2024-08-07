b996e019b69709ba99418378ca76b7fff191ccb9
mila811230 <bluegalaxy1230@gmail.com>
2024-08-07 17:21:12 +0900

    0807마무리

---
+++ src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,12 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		// 변수선언
+		int value = 0;
+		int result = value + 10;
+		System.out.println(result);
+	}
+
+}
---
+++ src/ch02/sec02/LongExample.java
@@ -0,0 +1,26 @@
+package ch02.sec02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+//		long var1 = 10;
+//		long var2 = 20L;
+//		long var3 = 1000000000000L;
+//		long var4 = 1000000000000L;
+//		
+//		System.out.println(var1);
+//		System.out.println(var2);
+//		System.out.println(var3);
+//		System.out.println(var4);
+		int x = 0xb3;
+		int y = 0b10100;
+		int z = 0x2a0f;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+
+	}
+
+}
